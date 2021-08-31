b7fe8fb6122dffdb69dfdb08bd7085f07270b52f
Joseph Congdon <josephcongdon92@gmail.com>
2021-08-31 09:55:39 -0500

    movies.title works

--- js/movies.js
+++ js/movies.js
@@ -1,4 +1,26 @@
 fetch('https://spangled-capable-lightning.glitch.me/movies')
 .then(response => response.json())
-.then(data => console.log(data))
-.catch(console.error)
+.then(data => formatMovies(data))
+.catch(console.error);
+
+
+function formatMovies(movies){
+    let html = ""
+    movies.forEach((movie, index ,arr) =>{
+         html += '<p>' + movie.title + '</p>'
+    })
+    $('#movie-list').html(html);
+}
+
+/// what are we trying to do?
+
+/// display a loading message
+
+/// finished list of movies
+
+/// ability to add movies
+
+/// ability to delete movies
+
+///what is the mvp?
+///what can we add to the project?
