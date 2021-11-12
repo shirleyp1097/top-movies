95ecb71356098f84f27e72512018c98301bbf9d0
Paris Shirley <shirleyp1097@gmail.com>
2021-11-12 13:58:32 -0600

    reformatted

--- js/movies.js
+++ js/movies.js
@@ -1,124 +1,231 @@
+const url = 'https://spangled-capable-lightning.glitch.me/movies';
 
 
-const url = 'https://spangled-capable-lightning.glitch.me/movies'
+$(document).ready(function() {
+    // INITIALIZATION
+    (function startPage() {
+        fetch(url)
+            .then(response => response.json())
+            .then($('#loading').html(""))
+            .then(() => displayMovies())
+            .catch(console.error)
+    })();
 
-// INITALIZATION
-fetch(url)
-.then(response => response.json())
-.then($('#loading').html(""))
-.then(function(data) {
-    console.log(data);
-    formatMovies(data);
-})
-.catch(console.error)
-
-
-
-
-
-function formatMovies(movies){
-    let html = ""
-    movies.forEach((movie, index ,arr) =>{
-        // if(movie.hasOwnProperty('title')) {
-        //     html += '<h4>' + movie.title + '</h4>' +
-        //         '<p>' + movie.rating + '/5' + '</p>'
-        // }
-        html += '<h4>' + movie.title + '</h4>' +
-                '<p>' + movie.rating + '/5' + '</p>' +
-            `<button class="delete-button" id=${movie.id}>` + 'Delete Movie' + '</button>'
-        // console.log(movie.id);
-
-        })
-
-
-    $('#movie-list').html(html);
-
-}
-
-
-$(document).on('click','.delete-button', function() {
-
-    let buttonNumber = $(this).attr('id')
-
-    console.log(buttonNumber);
-
-    const options = {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json'
-        }
+    function displayMovies() {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => formatMovies(data))
+            .catch(console.error)
     }
 
-    fetch(url + '/' + String(buttonNumber), options)
-        .then(response => console.log(response.json()))
-        .then(displayMovies)
-        .catch(console.error);
-});
+    function formatMovies(movies){
+        let html = ""
+        movies.forEach((movie, index ,arr) => {
+            html += '<h4>' + movie.title + '</h4>' +
+                '<p>' + movie.rating + '/5' + '</p>' +
+                `<button class="delete-button" id=${movie.id}>` + 'Delete Movie' + '</button>'
+        })
+        $('#movie-list').html(html);
+    }
 
-$('#submit-button').click(function(e){
-    e.preventDefault()
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            title:$('#user-movie').val(),
-            rating:$('#user-rating').val()
-        }),
-    };
+    // DELETE FUNCTIONALITY
+    $(document).on('click','.delete-button', function() {
 
-    fetch(url, options)
-        .then(displayMovies)
-        .catch(console.error);
-})
+        let buttonNumber = $(this).attr('id')
 
+        console.log(buttonNumber);
 
-function displayMovies() {
-    fetch(url)
-        .then(response=> response.json())
-        .then(data =>
-        {formatMovies(data);
-            console.log(data)})
-        .catch(console.error)
-}
+        const options = {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        fetch(url + '/' + String(buttonNumber), options)
+            .then(response => console.log(response.json()))
+            .then(() => displayMovies())
+            .catch(console.error);
+    });
 
 
-fetch(url)
-    .then(response => response.json())
-    .then(movies => {
-        let html = "";
-        html += '<select>'
-        movies.forEach((movie) =>
-            html += `<option value=${movie.id}>`+ movie.title + '</option>'
-        )
-        html += '</select>'
-        $('#all_movies').html(html)
+    $('#submit-button').click(function(e){
+        e.preventDefault()
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                title:$('#user-movie').val(),
+                rating:$('#user-rating').val()
+            }),
+        };
+
+        fetch(url, options)
+            .then(displayMovies)
+            .catch(console.error);
     })
 
-$('#submit-edit').click(function(e) {
-    e.preventDefault();
-    let originalMovieName = $('#all_movies').val();
-    let changedMovieName = $('#new-movie-title').val();
-    let changedMovieRating = $('#new-user-rating').val();
+
+
+    fetch(url)
+        .then(response => response.json())
+        .then(movies => {
+            let html = "";
+            html += '<select>'
+            movies.forEach((movie) =>
+                html += `<option value=${movie.id}>`+ movie.title + '</option>'
+            )
+            html += '</select>'
+            $('#all_movies').html(html)
+        })
+
+    $('#submit-edit').click(function(e) {
+        e.preventDefault();
+        let originalMovieName = $('#all_movies').val();
+        let changedMovieName = $('#new-movie-title').val();
+        let changedMovieRating = $('#new-user-rating').val();
 
 
 
-    const options = {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            title: changedMovieName,
-            rating: changedMovieRating
-        }),
-    };
+        const options = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                title: changedMovieName,
+                rating: changedMovieRating
+            }),
+        };
 
-    fetch(url + '/' + originalMovieName, options)
-        .then(displayMovies)
-        .catch(console.error);
+        fetch(url + '/' + originalMovieName, options)
+            .then(displayMovies)
+            .catch(console.error);
+    })
 })
+//     (function startPage() {
+//     fetch(url)
+//         .then(response => response.json())
+//         .then($('#loading').html(""))
+//         .then(() => displayMovies())
+//         .catch(console.error)
+//     })();
+//
+// // fetch(url)
+// //     .then(response => response.json())
+// //     .then($('#loading').html(""))
+// //     .then(() => displayMovies())
+// //     .catch(console.error)
+//
+// function displayMovies() {
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(data => formatMovies(data))
+//         .catch(console.error)
+// }
+//
+// function formatMovies(movies){
+//     let html = ""
+//     movies.forEach((movie, index ,arr) => {
+//         html += '<h4>' + movie.title + '</h4>' +
+//                 '<p>' + movie.rating + '/5' + '</p>' +
+//             `<button class="delete-button" id=${movie.id}>` + 'Delete Movie' + '</button>'
+//         })
+//     $('#movie-list').html(html);
+// }
+//
+//
+// $(document).on('click','.delete-button', function() {
+//
+//     let buttonNumber = $(this).attr('id')
+//
+//     console.log(buttonNumber);
+//
+//     const options = {
+//         method: 'DELETE',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     }
+//
+//
+//     // ORIGINAL WORKING
+//     fetch(url + '/' + String(buttonNumber), options)
+//         .then(response => console.log(response.json()))
+//         .then(() => displayMovies())
+//         .catch(console.error);
+//
+//     // fetch(url + '/' + String(buttonNumber), options)
+//     //     .then(response => console.log(response.json()))
+//     //     .then(
+//     //         () => fetch(url)
+//     //             .then(response=> response.json())
+//     //             .then(data =>
+//     //             {formatMovies(data);
+//     //                 console.log(data)})
+//     //             .catch(console.error)
+//     //     )
+//     //     .catch(console.error);
+//
+// });
+//
+// $('#submit-button').click(function(e){
+//     e.preventDefault()
+//     const options = {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//             title:$('#user-movie').val(),
+//             rating:$('#user-rating').val()
+//         }),
+//     };
+//
+//     fetch(url, options)
+//         .then(displayMovies)
+//         .catch(console.error);
+// })
+//
+//
+//
+// fetch(url)
+//     .then(response => response.json())
+//     .then(movies => {
+//         let html = "";
+//         html += '<select>'
+//         movies.forEach((movie) =>
+//             html += `<option value=${movie.id}>`+ movie.title + '</option>'
+//         )
+//         html += '</select>'
+//         $('#all_movies').html(html)
+//     })
+//
+// $('#submit-edit').click(function(e) {
+//     e.preventDefault();
+//     let originalMovieName = $('#all_movies').val();
+//     let changedMovieName = $('#new-movie-title').val();
+//     let changedMovieRating = $('#new-user-rating').val();
+//
+//
+//
+//     const options = {
+//         method: 'PUT',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//             title: changedMovieName,
+//             rating: changedMovieRating
+//         }),
+//     };
+//
+//     fetch(url + '/' + originalMovieName, options)
+//         .then(displayMovies)
+//         .catch(console.error);
+// })
 
 
 
